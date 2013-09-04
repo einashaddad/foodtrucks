4b7751c1c1e90382f47c782e317afcfcc5352fda
Einas Haddad <einas@haddad>
2013-09-04 00:47:31 -0700

    Add port to flask

--- foodtrucks.py
+++ foodtrucks.py
@@ -88,4 +88,5 @@ def distance(loc1, loc2):
     return sqrt((y2-y1)**2 + (x2-x1)**2)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
